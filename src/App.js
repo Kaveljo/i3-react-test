2583f5803265c0717741e7cfdac937248bb8e387
kaveljo <kaveljo@gmail.com>
2021-03-10 11:32:29 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
 
 import Home from './components/pages/Home'
 import About from './components/pages/About'
@@ -58,8 +57,7 @@ function App() {
             <Home cards={cards}/>
           </>
         )} />
-        <Route path="/about" component={() => <About aboutCards={aboutCards} />} /> 
-        {/* <Footer/> */}
+        <Route path="/about" component={() => <About aboutCards={aboutCards} />} />
       </div>
     </Router>
   );
